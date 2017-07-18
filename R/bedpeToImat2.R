aeca401ebf0c46fec651f957ee4dc82fe6000912
Daniel Vera <vera@genomics.fsu.edu>
2017-07-17 23:18:38 -0400

    Update bedpeToImat2.R

--- R/bedpeToImat2.R
+++ R/bedpeToImat2.R
@@ -1,30 +1,44 @@
-bedpeToImat <- function(bedFile , binsize , prefix , minInteractions=1 , threads=getOption("threads",1L) ){
+bamToImat <- function(bedFile , binsize , minInteractions=1 , minQual=20 , threads=getOption("threads",1L) ){
 
   require(Matrix)
 
-  if(missing(prefix)){ prefix <- basename(removeext(bedFile)) }
-  #tripletName <- paste0(basename(removeext(bedFile)),".triplet")
-
-
-  chroms <- sort(as.character(unlist(cmdRun(paste("cut -f 1",bedFile,"| uniq"),lines=T))))
+  numfiles <- length(bedFile)
+  
+  chroms <- sort(as.character(unlist(cmdRun(paste("cut -f 1",bedFile[1],"| uniq"),lines=T))))
   chroms <- chroms[which(chroms!="chrY")]
-  cmdString <- paste0(
-    "awk '{if($1==\"",chroms,"\" && $4== \"",chroms,"\" ){print $1,int($2/",binsize,"),int($5/",binsize,")}}' ", bedFile," | awk '{if($2<$3){print $1,$2,$3} else{print $1,$3,$2}}' OFS='\t' | sort -T . -S 10G -k1,1 -k2,2n -k3,3n | uniq -c | awk '{if($1>=",minInteractions,"){print $3,$4,$1}}' OFS='\t'"
+
+  outnames <- lapply(1:numfiles,function(x){
+    paste0(basename(removeext(bedFile[x])),"_q",minQual,"_min",minInteractions,"_",chroms,"_w",binsize,".imat")
+  })
+
+  
+  cmdStrings <- paste(
+    "bedtools bamtobed -bedpe -i",bedFile," | awk '{",
+      "if($1==$4 && $8 >= ",minQual,"){",
+        "left=int($2/",binsize,");",
+        "right=int($6/",binsize,");",
+        "if(left<right){print $1,left,right}",
+        "else{print $1,right,left}",
+      "}' OFS='\t' | sort -T . -S 10G -k1,1 -k2,2n -k3,3n | uniq -c | awk '{",
+        "if($1>=",minInteractions,"){",
+          "print $2,$3,$4,$1",
+        "}",
+      "}' OFS='\t'"
   )
 
   res <- cmdRun(cmdString,threads=threads,tsv=T)
-  names(res)=chroms
 
-  outnames <- paste0(prefix,"_",chroms,"_w",binsize,".imat")
-
-  # convert to coord tables to matrices
-  res <- lapply(res,data.matrix)
-
-  matlist = mclapply(seq_along(chroms),function(m){
-    numbins <- max(res[[m]][,1:2])
-    spMatrix(numbins,numbins,res[[m]][,1],res[[m]][,2],res[[m]][,3])
-  }, mc.cores=threads, mc.preschedule=FALSE)
-
-  names(matlist) <- chroms
+  res <- mclapply(1:numfiles,function(x){
+    y=split(res[[x]],res[[x]][,1])
+    y=lapply(y,"[",-1)
+    y=lapply(y,data.matrix)
+    matlist = lapply(seq_along(chroms),function(m){
+      numbins <- max(y[[m]][,1:2])
+      spMatrix(numbins,numbins,y[[m]][,1],y[[m]][,2],y[[m]][,3])
+    })
+    return(matlist)
+  },mc.cores=threads)
+  
+  names(res) <- bedFile
   return(matlist)
 }
