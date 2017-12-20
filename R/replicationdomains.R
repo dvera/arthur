b56a770276427056569f92c6d40c5f5297a56210
Daniel <dvera@bio.fsu.edu>
2017-12-20 16:45:58 -0500

    various

--- R/replicationdomains.R
+++ R/replicationdomains.R
@@ -1,32 +1,23 @@
 replicationdomains <-
-function(bgfile , lspan = 0 , mergewithin = 100000 , removeY = T ){
-	
-	
+function(bgfile ){
+
+
 	ttrname<-paste0(basename(removeext(bgfile)),"_ttr.bed")
 	rdname<-paste0(basename(removeext(bgfile)),"_rds.bed")
-	
 
-	if(lspan != 0){
-		cat(basename(bgfile),": smoothing bedGraph\n")
-		r<-bg.loess(r,lspan=lspan)
-	}
-	
+
+
 	cat(basename(bgfile),": loading bedGraph\n")
-	r<-read.tsv(bgfile)
+	r<-tsvRead(bgfile)
 	rl<-nrow(r)
-	#gf<-read.tsv(genomefile)
-	
-	if(removeY==TRUE){
-		r<-r[which(r$V1 != "chrY"),]
-	}
 
 	cat(basename(bgfile),": finding TTRs\n")
-	
+
 	#find sign-changes
-	r$prod=c(r$V4[1:(rl-1)]*r$V4[2:rl],1)
-	
+	r$prod=c(r[,4][1:(rl-1)]*r[,4][2:rl],1)
+
 	#calculate slope at each point
-	slopes=diff(r$V4)/diff(r$V2)
+	slopes=diff(r[,4])/diff(r[,2])
 	r$slope=c(slopes,slopes[rl-1])
 	rm(slopes)
 
@@ -39,16 +30,22 @@ function(bgfile , lspan = 0 , mergewithin = 100000 , removeY = T ){
 	leftttrb<-unlist(lapply(1:length(signchanges), function(x) signchanges[x]-which(r$slopeprod[signchanges[x]:1]<0)[1]+1 ))
 	cat(basename(bgfile),": finding right TTR boundaries\n")
 	ritettrb<-unlist(lapply(1:length(signchanges), function(x) signchanges[x]+which(r$slopeprod[(signchanges[x]):(signchanges[length(signchanges)])]<0)[1]-1 ))
-	
+
 	#t<-data.frame(signchanges,c(NA,leftttrb),c(ritettrb,NA))
 	t<-data.frame(signchanges,leftttrb,ritettrb)
+
+
+
 
 	cat(basename(bgfile),": removing incomplete TTRs at chromosome ends\n")
 	cbreaks1 <- which(  r[t[,2],2] > r[t[,1],2]  )
 	if(length(cbreaks1)>0){ t[cbreaks1,2] <- NA }
-	
+
 	cbreaks2 <- which(  r[t[,3],2] < r[t[,1],2]  )
 	if(length(cbreaks2)>0){ t[cbreaks2,3] <- NA }
+
+	# t=t[complete.cases(t),]
+	# t=t[which(t[,2]!=t[,3]),]
 
 	cat(basename(bgfile),": finding TTR directionality\n")
 	t$strand <- NA
@@ -62,11 +59,11 @@ function(bgfile , lspan = 0 , mergewithin = 100000 , removeY = T ){
 
 	cat(basename(bgfile),": saving RDs to ",rdname,"\n")
 	rd<-data.frame("V1"=r[t[1:(nrow(t)-1),1],1],"V2"=r[t[1:(nrow(t)-1),1],2],"V3"=r[t[2:(nrow(t)),1],2], "V4"=1:(nrow(t)-1) , "V5"=1 , "V6"=t[2:(nrow(t)),4] , "V7"=r[t[1:(nrow(t)-1),3],2],"V8"=r[t[2:(nrow(t)),2],3])
-	rd<-rd[which(rd$V3>rd$V2),]
+	rd<-rd[which(rd[,3]>rd[,2]),]
 	rd<-rd[which(complete.cases(rd)),]
 	write.tsv(rd,file=rdname)
 	return(c(ttrname,rdname)
-	#chromends<-prerd[which(prerd$V3<prerd$V2),]
+	#chromends<-prerd[which(prerd[,3]<prerd[,2]),]
 	#chrombegs<-chromends
 	#chrombegs[,2]<-1
 	#chromends[,3]<-gf[match(chromends$V1,gf$V1),2]
---
+++ R/toePrint.R
@@ -0,0 +1,131 @@
+toePrint <- function( bgFilesList , threads=getOption("threads",1L) )
+
+require(travis)
+
+numFiles <- lapply(bgFilesList,length)
+numSets <- length(bgFilesList)
+fileIndex <- rep(1:numSets,numFiles)
+allFiles <- unlist(bgFilesList)
+if(any(grepl(" ",allFiles))){stop("I hate spaces in file names, quiting.")}
+setNames <- names(bgFilesList)
+cat("counting file lines...\n")
+allFileLines <- filelines(allFiles,threads=threads)
+
+if(!is.list(bgFilesList) || numSets<2){
+  stop("ERROR: bgFilesList should be a list of vectors of paths to bedGraph files")
+}
+
+if(length(unique(setNames))!=numSets){
+  stop("must name lists of files uniquely")
+}
+
+# check if files have consistent rows
+if(length(unique(allFileLines))>1){
+  cat("files have inconsistent rows, unifying files\n")
+  allFiles <- bgUnify(allFiles)
+  newFileLines <- filelines(allFiles,threads=threads)
+  bgFilesList <- lapply(1:numSets,function(x){ allFiles[which(fileIndex==x)] })
+  names(bgFilesList) <- setNames
+  print(data.frame(group=fileIndex,file=allFiles,linesBeforeUnifying=allFileLines,linesAfterUnifying=newFileLines,percentRemaining=100*newFileLines/allFileLines))
+}
+
+# define comparisons
+i=1:length(bgFilesList)
+eg=unique(t(apply(expand.grid(i,i),1,sort)))
+eg=eg[which(eg[,1]!=eg[,2]),]
+nm=eg
+nm[] <- setNames[nm]
+numComps <- nrow(eg)
+
+# file names
+compNames <- paste0(nm[,1],"-vs-",nm[,2])
+bedNames <- paste0(compNames,".bed")
+bgNames <- paste0(compNames,"_phred.bg")
+pdfNames <- paste0(compNames,".pdf")
+
+
+
+cat("reading ",numFiles," bedGraph files...\n")
+bgs <- tsvRead(allFiles,threads=threads)
+bgl <- as.data.frame(lapply(bgs,"[",4))
+numRows <- nrow(bgl)
+names(bgl) <- basename(removeext(allFiles))
+names(bgs) <- setNames
+sleepTimes <- sort(1:numComps)/100
+
+wtf=lapply(1:numComps,function(compNum){
+
+  Sys.sleep(sleepTimes[compNum])
+  cat(compNames[compNum],": starting...\n")
+
+  group1=which(fileIndex==eg[compNum,1])
+  group2=which(fileIndex==eg[compNum,2])
+
+  bgl1=bgl[,group1]
+  bgl2=bgl[,group2]
+
+  numSamples1=length(group1)
+  numSamples2=length(group2)
+
+  index1=1:numSamples1
+  index2=(numSamples1+1):sum(numSamples1+numSamples2)
+  numDists1=((numSamples1^2)-numSamples1)
+  numDists2=((numSamples2^2)-numSamples2)
+  numDists=numSamples1*numSamples2
+
+
+  results <- as.data.frame(t(as.data.frame(lapply(1:numRows,function(x){
+    allDistances    <-  as.matrix(dist(bgl[x,c(group1,group2),drop=T]))
+    withinDist1  <-  allDistances[index1,index1]
+    withinDist2  <-  allDistances[index2,index2]
+    acrossDist   <-  allDistances[index1,index2]
+    within_mean1 <- sum(withinDist1) / numDists1
+    within_mean2 <- sum(withinDist2) / numDists2
+    across_mean  <- sum(acrossDist)  / numDists
+    #withinMeanMax <- max(c(within_mean1,within_mean2))
+    withinMeanMean <- mean(c(within_mean1,within_mean2))
+    localPvalue <- t.test(c(as.vector(withinDist1),as.vector(withinDist2)),as.vector(acrossDist))$p.value
+    #scorePvalue <- t.test(as.vector(bgl[x,group1]),as.vector(bgl[x,group2]))$p.value
+
+    res <- c(
+      rowNum         = x,
+      within_mean1   = within_mean1,
+      within_mean2   = within_mean2,
+      acrossMeanDist = across_mean,
+      #withinMeanMax  = withinMeanMax,
+      withinMeanMean = withinMeanMean,
+      localPvalue    = localPvalue
+      #scorePvalue    = scorePvalue
+    )
+    return(res)
+  }))))
+
+  rownames(results)=NULL
+  results <- results[which(results$localPvalue <=0.05),]
+  results$localQvalue=p.adjust(results$localPvalue)
+  withinCdf <- ecdf(results$withinMeanMean)
+  results$globalPvalue <- unlist(lapply(results$acrossMeanDist,withinCdf))
+  results$globalPvalue <- 1-results$globalPvalue
+  results$globalQvalue <- p.adjust(results$globalPvalue)
+  results$localQvalue  <- p.adjust(results$localPvalue)
+
+  rgbm <- matrix(rep(0,(3*numRows)),ncol=3)
+  lowLocQ <- which(results$localQvalue  <= 0.05)
+  lowGloQ <- which(results$globalQvalue <= 0.05)
+  lowGloP <- which(results$globalPvalue <= 0.05)
+
+  rgbm[lowLocQ,1]=0.7
+  rgbm[lowGloP,3]=0.7
+  colors=unlist(lapply(1:numRows,function(r){ do.call(rgb,as.list(rgbm[r,])) }))
+
+
+
+  cat(compNames[compNum],": finished identifying regions, starting to save output...\n")
+
+  pdf(file=pdfNames)
+  rageHist(bgl[,c(group1,group2)])
+  plot()
+
+
+})
+}
